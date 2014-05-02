25bbce573b55b5f32c680b0dd1e36d626c9c5c46
skebutz <snyggkille18@gmail.com>
2014-05-02 20:13:09 +0200

    Update keymap_poker.c

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -1,7 +1,7 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: qwery */
+    /* 0: qwerty */
     KEYMAP(ESC,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS,DELETE,HOME, END,PGDN,PGUP, \
            TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,BSLS,  RBRC,  P7,  P8,  P9,PSLS, \
            NO,     A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,  NO,  SLSH,  P4,  P5,  P6,PAST, \
@@ -10,16 +10,16 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            
     /* 1: Cat */   
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PSCR,PAUS,NLCK,SLCK, \
-           NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  MS_U,BTN2,GRV, NUBS,NO,  NO,  NO,  NO, \
-           CAPS,VOLD,VOLU,MUTE,NO,  NO,  NO,  NO,  MS_L,MS_D,MS_R,NO,  NO,  NO,  NO,  NO,  NO, \
-           LSFT,NO,  NO,  NO,  NO,  NO,  NO,  NO,  BTN1,WH_D,WH_U,NO,  NO,  NO,  NO,  NO,  NO, \
+           NO,  NO,  NO,  NO,  NO,  NO,  NO,  ACL2,  NO,  MS_U,BTN2,GRV, NUBS,NO,  NO,  NO,  NO, \
+           CAPS,VOLD,VOLU,MUTE,NO,  NO,  NO,  ACL1,  MS_L,MS_D,MS_R,NO,  NO,  NO,  NO,  NO,  NO, \
+           LSFT,NO,  NO,  NO,  NO,  NO,  NO,  ACL0,  BTN1,WH_D,WH_U,NO,  NO,  NO,  NO,  NO,  NO, \
            NO,  LALT,NO,  NO,  DEL, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO),
            
-    /* Key */       
+    /* 2: Key */       
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, INS, PSCR,PAUS,NLCK,SLCK, \
-           NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  MS_U,BTN2,GRV, NUBS,NO,  NO,  NO,  NO, \
-           CAPS,VOLD,VOLU,MUTE,NO,  NO,  NO,  NO,  MS_L,MS_D,MS_R,NO,  NO,  NO,  NO,  NO,  NO, \
-           LSFT,NO,  NO,  NO,  NO,  NO,  NO,  NO,  BTN1,WH_D,WH_U,NO,  NO,  NO,  NO,  NO,  NO, \
+           NO,  NO,  NO,  NO,  NO,  NO,  NO,  ACL2,  NO,  MS_U,BTN2,GRV, NUBS,NO,  NO,  NO,  NO, \
+           CAPS,VOLD,VOLU,MUTE,NO,  NO,  NO,  ACL1,  MS_L,MS_D,MS_R,NO,  NO,  NO,  NO,  NO,  NO, \
+           LSFT,NO,  NO,  NO,  NO,  NO,  NO,  ACL0,  BTN1,WH_D,WH_U,,  NO,  NO,  NO,  NO,  NO, \
            NO,  LALT,NO,  NO,  DEL, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO),
 };
 const uint16_t PROGMEM fn_actions[] = {
