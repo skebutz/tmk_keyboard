e5fa39e31a2fbd31d336472f5bb55e914c1a14e6
skebutz <snyggkille18@gmail.com>
2014-05-02 21:08:28 +0200

    Update matrix.c

--- keyboard/gh60/matrix.c
+++ keyboard/gh60/matrix.c
@@ -120,7 +120,7 @@ void matrix_print(void)
     print("\nr/c 0123456789ABCDEFG\n");
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
         phex(row); print(": ");
-        pbin_reverse17(matrix_get_row(row));
+        pbin_reverse16(matrix_get_row(row));
         print("\n");
     }
 }
@@ -171,7 +171,7 @@ static matrix_row_t read_cols(void)
            (PINB&(1<<6) ? 0 : (1<<12)) |
            (PINB&(1<<5) ? 0 : (1<<13)) |
            (PINB&(1<<4) ? 0 : (1<<14)) |
-           (PIND&(1<<7) ? 0 : (1<<15)) |
+           (PIND&(1<<7) ? 0 : (1UL<<15)) |
            (PIND&(1<<4) ? 0 : (1UL<<16));
 }
 
